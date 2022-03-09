2eae4541600adf094ef231e4803a85f314b58cfc
Patrick Guo <guopatrick.courses@gmail.com>
2022-03-08 16:35:27 -0800

    Update index.js

--- NPM business card/index.js
+++ NPM business card/index.js
@@ -8,7 +8,6 @@ console.log('[32m[39m\n\
 [32m   │   [37m[1mLinkedIn:[22m[39m[32m  [90mhttps://linkedin.com/in/[39m[32m[94mpatrickguo[39m[32m               │[39m\n\
 [32m   │   [37m[1m  GitHub:[22m[39m[32m  [90mhttps://github.com/[39m[32m[32mshpatrickguo[39m[32m                  │[39m\n\
 [32m   │   [37m[1m     npm:[22m[39m[32m  [90mhttps://npmjs.com/[39m[32m[31m~shpatrickguo[39m[32m                  │[39m\n\
-[32m   │   [37m[1mFacebook:[22m[39m[32m  [90mhttps://facebook.com/[39m[32m[34mguopatrick[39m[32m                  │[39m\n\
 [32m   │   [37m[1m    Card:[22m[39m[32m  [31mnpx[39m[32m [37mshpg-card[39m[32m                                    │[39m\n\
 [32m   │                                                               │[39m\n\
 [32m   │                                                               │[39m\n\
