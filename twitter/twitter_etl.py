864f528186723c421adb4c1c6654c84f669ae9a6
Patrick Guo <guopatrick.courses@gmail.com>
2023-06-05 12:14:27 -0700

    Update twitter_etl.py

--- twitter/twitter_etl.py
+++ twitter/twitter_etl.py
@@ -6,39 +6,44 @@ import s3fs
 from dotenv import load_dotenv
 import os
 
-# load .env file
-load_dotenv()
+def run_twitter_etl():
 
-# get the access token
-access_key = os.getenv("access_key")
-access_secret = os.getenv("access_secret")
-consumer_key = os.getenv("consumer_key")
-consumer_secret = os.getenv("consumer_secret")
+    # load .env file
+    load_dotenv()
 
-# Twitter authentication
-auth = tweepy.OAuthHandler(access_key, access_secret)
-auth.set_access_token(consumer_key, consumer_secret)
+    # get the access token
+    access_key = os.getenv("access_key")
+    access_secret = os.getenv("access_secret")
+    consumer_key = os.getenv("consumer_key")
+    consumer_secret = os.getenv("consumer_secret")
 
-# Creating an API object
-api = tweepy.API(auth)
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)
+    auth.set_access_token(consumer_key, consumer_secret)
 
-tweets = api.user_timeline(screen_name='@elonmusk',
-                           # 200 is the maximum allowed count
-                           count = 200, 
-                           include_rts = False,
-                           # Necessary to keep full_tex
-                           # otherwise only the first 140 words are extracted                        
-                           tweet_mode = 'extended'
-                           )
-# Convert raw tweets
-tweet_list = []
-for tweet in tweets:
-    text = tweet._json["full_text"]
+    # Creating an API object
+    api = tweepy.API(auth)
+    USER = '@OpenAI'
+    tweets = api.user_timeline(screen_name=USER,
+                            # 200 is the maximum allowed count
+                            count = 200, 
+                            include_rts = False,
+                            # Necessary to keep full_tex
+                            # otherwise only the first 140 words are extracted                        
+                            tweet_mode = 'extended'
+                            )
+    # Convert raw tweets to dataframe
+    tweet_list = []
+    for tweet in tweets:
+        text = tweet._json["full_text"]
 
-    refined_tweet = {"user": tweet.user.screen_name,
-                     "text": text,
-                     "favorite_count": tweet.favorite_count,
-                     "retweet_count": tweet.retweet_count,
-                     "created_at": tweet.created_at
-                    }
-    tweet_list.append(refined_tweet)
+        refined_tweet = {"user": tweet.user.screen_name,
+                        "text": text,
+                        "favorite_count": tweet.favorite_count,
+                        "retweet_count": tweet.retweet_count,
+                        "created_at": tweet.created_at
+                        }
+        tweet_list.append(refined_tweet)
+
+    df = pd.DataFrame(tweet_list)
+    df.to_csv(f"{USER}_tweets.csv")
