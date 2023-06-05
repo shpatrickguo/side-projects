39dd6940872f66146ab01688f2952fb011c28002
Patrick Guo <guopatrick.courses@gmail.com>
2023-06-05 11:57:21 -0700

    Update twitter_etl.py

--- twitter/twitter_etl.py
+++ twitter/twitter_etl.py
@@ -19,4 +19,26 @@ consumer_secret = os.getenv("consumer_secret")
 auth = tweepy.OAuthHandler(access_key, access_secret)
 auth.set_access_token(consumer_key, consumer_secret)
 
-# Twitter API
+# Creating an API object
+api = tweepy.API(auth)
+
+tweets = api.user_timeline(screen_name='@elonmusk',
+                           # 200 is the maximum allowed count
+                           count = 200, 
+                           include_rts = False,
+                           # Necessary to keep full_tex
+                           # otherwise only the first 140 words are extracted                        
+                           tweet_mode = 'extended'
+                           )
+# Convert raw tweets
+tweet_list = []
+for tweet in tweets:
+    text = tweet._json["full_text"]
+
+    refined_tweet = {"user": tweet.user.screen_name,
+                     "text": text,
+                     "favorite_count": tweet.favorite_count,
+                     "retweet_count": tweet.retweet_count,
+                     "created_at": tweet.created_at
+                    }
+    tweet_list.append(refined_tweet)
